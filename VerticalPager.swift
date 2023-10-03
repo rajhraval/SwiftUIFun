5bee0f557627889619fc9bda674422f6d5fe3856
Raj H Raval <rajraval37@gmail.com>
2023-10-03 21:41:36 +0530

    Create VerticalPager.swift

---
+++ VerticalPager.swift
@@ -0,0 +1,43 @@
+/// It's just an example
+struct VerticalPager: View {
+    
+    let colors: [Color] = [.pink, .orange, .indigo, .green]
+    let morningAffirmations = [
+        "I am grateful for a new day filled with endless opportunities.",
+        "I am confident in my abilities and ready to tackle any challenges that come my way.",
+        "I radiate positivity and attract positive energy into my life.",
+        "I embrace change as a chance to grow and learn."
+    ]
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView(.vertical) {
+                VStack(spacing: 0) {
+                    ForEach(0..<colors.count) { color in
+                        ZStack {
+                            Rectangle()
+                                .fill(colors[color])
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("Affirmation \(color + 1).")
+                                    .font(.system(size: 36).bold())
+                                    .foregroundStyle(.white.opacity(0.7))
+                                Text(morningAffirmations[color])
+                                    .font(.system(size: 48).bold())
+                                    .foregroundStyle(.white)
+                            }
+                            .padding()
+                        }
+                        .frame(height: geo.size.height)
+                    }
+                }
+            }
+            .scrollTargetBehavior(.paging)
+            .scrollIndicators(.hidden)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    VerticalPager()
+}
